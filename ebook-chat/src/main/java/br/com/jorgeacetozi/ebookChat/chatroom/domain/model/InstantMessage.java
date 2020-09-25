9fbbe8f56550e8d3518346575147a56be48501b6
Recai Furkan Bostancı <30552187+recaifurkan@users.noreply.github.com>
2020-09-25 21:55:54 +0300

    fixed bugs

--- ebook-chat/src/main/java/br/com/jorgeacetozi/ebookChat/chatroom/domain/model/InstantMessage.java
+++ ebook-chat/src/main/java/br/com/jorgeacetozi/ebookChat/chatroom/domain/model/InstantMessage.java
@@ -16,7 +16,7 @@ public class InstantMessage {
 	@PrimaryKeyColumn(name = "username", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
 	private String username;
 	
-	@PrimaryKeyColumn(name = "chatRoomId", ordinal = 1, type = PrimaryKeyType.PARTITIONED)
+	@PrimaryKeyColumn(name = "chatroomid", ordinal = 1, type = PrimaryKeyType.PARTITIONED)
 	private String chatRoomId;
 	
 	@PrimaryKeyColumn(name = "date", ordinal = 2, type = PrimaryKeyType.CLUSTERED, ordering = Ordering.ASCENDING)
